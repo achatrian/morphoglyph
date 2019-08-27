68cb3ac6a20bb6e53bf26cb3e8a6d047fa4adfd1
achatrian <chatrian.andrea@gmail.com>
2019-08-27 15:51:45 +0100

    added shape selection for categorical variables + built

--- dist/phewLocal.js
+++ dist/phewLocal.js
@@ -31,73 +31,73 @@ const chalk = require('chalk')
 
 
 async function startServer () {
-  const app = express()
-  const port = 2999
+    const app = express()
+    const port = 2999
 
-  // Allow CORS
-  app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-    next()
-  })
+    // Allow CORS
+    app.use(function (req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*')
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+        next()
+    })
 
-  // Need to parse POST Body data (for parsing application/json)
-  // Increase the limit from the default (100kb) to enable large annotation JSON
-  // files
-  app.use(bodyParser.json({
-    limit: '100mb'
-  }))
+    // Need to parse POST Body data (for parsing application/json)
+    // Increase the limit from the default (100kb) to enable large annotation JSON
+    // files
+    app.use(bodyParser.json({
+        limit: '100mb'
+    }))
 
-  // // Save annoation data
-  // app.post('/save', async function (req, res) {
-  //   try {
-  //     await saveAnnotation(req.body)
-  //     res.send('Success, annotation data saved')
-  //   } catch (err) {
-  //     console.log('Data could not be saved')
-  //     console.log(err)
-  //     res.send('Failed, annotation data could not be saved')
-  //   }
-  // })
-  //
-  // // Check for images that may have been added to the directory
-  // app.post('/checkForTemplates', async function (req, res) {
-  //   try {
-  //     await checkForTemplates()
-  //     res.send('Success, found templates')
-  //   } catch (err) {
-  //     console.log('Could not check for templates')
-  //     console.log(err)
-  //     res.send('Failed, could not find templates')
-  //   }
-  // })
+    // // Save annoation data
+    // app.post('/save', async function (req, res) {
+    //   try {
+    //     await saveAnnotation(req.body)
+    //     res.send('Success, annotation data saved')
+    //   } catch (err) {
+    //     console.log('Data could not be saved')
+    //     console.log(err)
+    //     res.send('Failed, annotation data could not be saved')
+    //   }
+    // })
+    //
+    // // Check for images that may have been added to the directory
+    // app.post('/checkForTemplates', async function (req, res) {
+    //   try {
+    //     await checkForTemplates()
+    //     res.send('Success, found templates')
+    //   } catch (err) {
+    //     console.log('Could not check for templates')
+    //     console.log(err)
+    //     res.send('Failed, could not find templates')
+    //   }
+    // })
 
-  // Serve image files
-  app.use('/templates', express.static('data/templates', {})) // makes templates available at /template
+    // Serve image files
+    app.use('/templates', express.static('data/templates', {})) // makes templates available at /template
 
-  // // Serve static annotation data
-  // // Always respond with headers that disable the cache. Specifically this is
-  // // important when editing raster images. Without this, when the editor is
-  // // reloaded the browser will serve the original, pre-edit, from the cache.
-  // app.use('/annotations', express.static('data/annotations', {
-  //   setHeaders: function (res, path) {
-  //     res.set('Cache-Control', 'no-store, no-cache, must-revalidate, private')
-  //   }
-  // }))
+    // // Serve static annotation data
+    // // Always respond with headers that disable the cache. Specifically this is
+    // // important when editing raster images. Without this, when the editor is
+    // // reloaded the browser will serve the original, pre-edit, from the cache.
+    // app.use('/annotations', express.static('data/annotations', {
+    //   setHeaders: function (res, path) {
+    //     res.set('Cache-Control', 'no-store, no-cache, must-revalidate, private')
+    //   }
+    // }))
 
-  // Serve the built application
-  app.use(express.static('.'))
+    // Serve the built application
+    app.use(express.static('.'))
 
-  // Get the IP address of the current machine. The application will also be
-  // usable over the network from this address.
-  const networkIPAddress = ip.address()
+    // Get the IP address of the current machine. The application will also be
+    // usable over the network from this address.
+    const networkIPAddress = ip.address()
 
-  // Listen to requests
-  app.listen(port, () => {
-    console.log(`  PHEW running at:`)
-    console.log('  - Local:   ' + chalk.cyan(`http://localhost:${port}/`))
-    console.log('  - Network: ' + chalk.cyan(`http://` + networkIPAddress + `:${port}/`))
-  })
+    // Listen to requests
+    app.listen(port, () => {
+        console.log(`  PHEW running at:`)
+        console.log('  - Local:   ' + chalk.cyan(`http://localhost:${port}/`))
+        console.log('  - Network: ' + chalk.cyan(`http://` + networkIPAddress + `:${port}/`))
+    })
 }
 
 startServer()
