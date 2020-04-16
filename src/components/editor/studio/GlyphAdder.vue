4d134d98c4dba340d853859c0ba0f9fedfeff8ee
achatrian <chatrian.andrea@gmail.com>
2020-04-16 19:06:22 +0100

    Vizprop color picker changes filling as well. Added defaults for bind options. Nucleus scales automatically with cell glyph when its dimensions aren't assigned Fixed decoration bug that prevented drawing when glyph had children Removed tooltips that hid chart controller buttons And probably much more ...

---
+++ src/components/editor/studio/GlyphAdder.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <v-card class="panel" flat>
+            <v-divider/>
+            <v-toolbar
+                    color="dark"
+                    flat
+                    dense>
+                <div class="select-title">
+                    <v-text-field placeholder="New glyph name" v-model="writtenGlyphName"/>
+                </div>
+            </v-toolbar>
+            <v-toolbar
+                    color="dark"
+                    flat
+                    dense>
+                <v-select
+                        class="selector"
+                        outlined
+                        :items="glyphTypeItems"
+                        label="Choose glyph type"
+                        v-model="selectedGlyphType"
+                />
+            </v-toolbar>
+        </v-card>
+    </div>
+</template>
+
+<script>
+import {mapState, mapActions} from 'vuex'
+
+export default {
+    name: "GlyphAdder",
+    data () {return{
+        writtenGlyphName: '',
+        selectedGlyphType: ''
+    }},
+    computed: {
+        ...mapState({
+            glyphTypes: state => state.glyph.glyphTypes
+        }),
+        glyphTypeItems () {
+            return this.glyphTypes.map(glyphType => glyphType.name.slice(0, -5))
+        }
+    }
+}
+</script>
+
+<style scoped>
+.panel {
+    margin-top: 0px;
+}
+.select-title{
+    position: relative;
+    margin-top: 5px
+}
+</style>
